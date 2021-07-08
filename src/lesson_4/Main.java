65e9cfddf9cb29918cbc2ae8c33de3dc3a9ecab3
DenisCheburov <Gamerss0o112@gmail.com>
2021-07-08 11:00:26 +0300

    HW_lesson_4

--- src/lesson_4/Main.java
+++ src/lesson_4/Main.java
@@ -146,17 +146,6 @@ public class Main {
 }
 
 
-//        if (map[0][0] == c && map[0][1] == c && map[0][2] == c){return true;}
-//        if (map[1][0] == c && map[1][1] == c && map[1][2] == c){return true;}
-//        if (map[2][0] == c && map[2][1] == c && map[2][2] == c){return true;}
-//
-//        if (map[0][0] == c && map[1][0] == c && map[2][0] == c){return true;}
-//        if (map[0][1] == c && map[1][1] == c && map[2][1] == c){return true;}
-//        if (map[0][2] == c && map[1][2] == c && map[2][2] == c){return true;}
-//
-//        if (map[0][0] == c && map[1][1] == c && map[2][2] == c){return true;}
-//        if (map[0][2] == c && map[1][1] == c && map[2][0] == c){return true;}
-
 
 
 
